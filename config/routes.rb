bafc446658e0603b3e2e0fe61578a477bc84b844
konomi1 <donnguri555@gmail.com>
2021-09-08 04:23:26 +0000

    [add]homes_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  root to: 'homes#top'
+
+  resources :homes, only: [:top] do
+    collection do
+      get 'about'
+    end
+  end
+
+
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
