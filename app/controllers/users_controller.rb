be98019b0d2cc1657f9790499f6b114913596a95
konomi1 <donnguri555@gmail.com>
2021-09-11 08:29:58 +0000

    [add]fixed_finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
     if @user.id != current_user.id
-      redirect_to user_path(@user)
+      redirect_to user_path(current_user)
     end
   end
 
