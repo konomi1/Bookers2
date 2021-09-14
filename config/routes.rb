726d7adf6d4e732a34875b7f83af42a6493cb5c0
konomi1 <donnguri555@gmail.com>
2021-09-14 06:04:47 +0000

    [Add]favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  root 'homes#top'
+  get "home/about", to: 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 
-  root 'homes#top'
-  get "home/about", to: 'homes#about'
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_08_063635) do
+ActiveRecord::Schema.define(version: 2021_09_14_045623) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "book_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
