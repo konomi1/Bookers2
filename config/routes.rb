0e85236cd81050d3ee6fa8465e822daf4fc34ec5
konomi1 <donnguri555@gmail.com>
2021-09-14 08:22:57 +0000

    [Add]comments

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show, :edit, :update]
   resources :books do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
 
