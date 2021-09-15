8740220e80f758fdcb34163d96c69cf987080a4b
konomi1 <donnguri555@gmail.com>
2021-09-15 12:03:07 +0000

    [add]follower1

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'homes#top'
-  get "home/about", to: 'homes#about'
+  get 'home/about', to: 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :books do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
