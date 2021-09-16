34b276883aecdc07e7569451ce761f0ad635a05f
konomi1 <donnguri555@gmail.com>
2021-09-16 07:45:49 +0000

    [finish]search

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'searches/search'
   devise_for :users
   root 'homes#top'
   get 'home/about', to: 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/search', to: 'searches#search'
 
   resources :users, only: [:index, :show, :edit, :update]do
     resource :relationships, only: [:create, :destroy]
