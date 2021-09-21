1e582d909db2d713847c0db2c73e610106fb4e0c
konomi1 <donnguri555@gmail.com>
2021-09-21 10:14:53 +0000

    [start]DM

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ApplicationRecord
 
   has_many :passive_relationships, class_name:"Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :passive_relationships , source: :follower
+  
+  has_many :entreis, dependent: :destroy
+  has_many :messages, dependent: :destroy
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
 
+  resources :room, only:[:create, :show]
+  resources :message, onli:[:create]
+
   resources :books do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
