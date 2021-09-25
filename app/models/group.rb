8b99ef37ac69521a4ba7ae4a23fd0f8a6bcb7777
konomi1 <donnguri555@gmail.com>
2021-09-25 10:34:13 +0000

    [fix]advancedlv7c

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   has_many :group_users, dependent: :destroy
+  has_many :users, through: :groups_users
 
   validates :name, presence: true
   validates :introduction, presence: true
